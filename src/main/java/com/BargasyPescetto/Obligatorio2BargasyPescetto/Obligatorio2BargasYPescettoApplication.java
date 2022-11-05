2355e816acb93be9ad2f24a69b6a6efc5b5c0786
angelop47 <pescettoangelo@gmail.com>
2022-11-05 16:40:24 -0300

    first commit

---
+++ src/main/java/com/BargasyPescetto/Obligatorio2BargasyPescetto/Obligatorio2BargasYPescettoApplication.java
@@ -0,0 +1,13 @@
+package com.BargasyPescetto.Obligatorio2BargasyPescetto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Obligatorio2BargasYPescettoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Obligatorio2BargasYPescettoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BargasyPescetto/Obligatorio2BargasyPescetto/Obligatorio2BargasYPescettoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BargasyPescetto.Obligatorio2BargasyPescetto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Obligatorio2BargasYPescettoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
